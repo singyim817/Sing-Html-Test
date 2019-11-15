febb98dfced58ac8df6d655ae188f11afe1613d3
Sing Yim <yimks2005@hotmail.com>
2019-11-15 17:55:29 +0800

    update fb login option

--- scripts/fb01.js
+++ scripts/fb01.js
@@ -13,37 +13,38 @@ ui.start('#firebaseui-auth-container', {
 var provider = new firebase.auth.FacebookAuthProvider();
 // provider.addScope('user_birthday');
 firebase.auth().useDeviceLanguage();
-// provider.setCustomParameters({
-//     'display': 'popup'
-//   });
 
-
-  function signInWithFB(){
-
-    firebase.auth().signInWithRedirect(provider);
-    firebase.auth().getRedirectResult().then(function(result) {
-      if (result.credential) {
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        var token = result.credential.accessToken;
-        // ...
-      console.log('successful.', result);
+var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
       }
-      // The signed-in user info.
-      var user = result.user;
-    }).catch(function(error) {
-      console.log('failed.', error);
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
-    
-  }
-
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: 'https://yimks.github.io/Sing-Html-Test/',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: ''
+  };
 
   function signOutFB() {
     firebase.auth().signOut().then(function() {
