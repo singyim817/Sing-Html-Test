323fd8782ac0ae7e841775c51c30e61fddfb0000
Sing Yim <yimks2005@hotmail.com>
2019-11-15 17:34:07 +0800

    Update fb login options

--- scripts/fb01.js
+++ scripts/fb01.js
@@ -1,25 +1,43 @@
 var provider = new firebase.auth.FacebookAuthProvider();
 // provider.addScope('user_birthday');
 firebase.auth().useDeviceLanguage();
-provider.setCustomParameters({
-    'display': 'popup'
-  });
+// provider.setCustomParameters({
+//     'display': 'popup'
+//   });
 
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    console.log('successful.', result);
-    // ...
-  }).catch(function(error) {
+
+  function signInWithFB(){
+
+    firebase.auth().signInWithRedirect(provider);
+    firebase.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // ...
+      console.log('successful.', result);
+      }
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function(error) {
       console.log('failed.', error);
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+    
+  }
+
+
+  function signOutFB() {
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
+        console.log('failed.', error);
+      });
+  }
